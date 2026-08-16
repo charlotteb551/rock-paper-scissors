87d4d75952f164f40457016b4f485d1c117ca716
charlotteb551 <295230572+charlotteb551@users.noreply.github.com>
2026-08-16 16:13:36 +0100

    Added computers random choice of rock,paper,scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+function getComputerChoice(){
+    const a=Math.random()
+    if (a<1/3){
+        result = "rock"
+    }else if (a<2/3){
+        result = "paper"
+    }else{
+        result = "scissors"
+    }
+    return result
+}
+
+console.log(getComputerChoice())

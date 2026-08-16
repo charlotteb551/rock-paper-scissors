f76c0c71bc22f21e6d36c3f87f0c4a7785fcce9d
charlotteb551 <295230572+charlotteb551@users.noreply.github.com>
2026-08-16 16:51:02 +0100

    Added functions to play a round and then play a game

--- javascript.js
+++ javascript.js
@@ -14,4 +14,59 @@ function getHumanChoice(){
     return b.toLowerCase();
 }
 
-console.log(getHumanChoice())
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice){
+        if(humanChoice === "rock"){
+            if(computerChoice === "rock"){
+                console.log("It's a draw!");
+            }else if(computerChoice === "paper"){
+                console.log("You lose!");
+                computerScore += 1;
+            }else{
+                console.log("You win!");
+                humanScore += 1;
+            }
+        }else if(humanChoice === "paper"){
+            if(computerChoice === "paper"){
+                console.log("It's a draw!");
+            }else if(computerChoice === "scissors"){
+                console.log("You lose!");
+                computerScore += 1;
+            }else{
+                console.log("You win!");
+                humanScore += 1;
+            }
+        }else{
+            if(computerChoice === "scissors"){
+                console.log("It's a draw!");
+            }else if(computerChoice === "rock"){
+                console.log("You lose!");
+                computerScore += 1;
+            }else{
+                console.log("You win!");
+                humanScore += 1;
+            }
+        }
+    }
+
+    for (let i=0; i<5;i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        console.log("You:"+humanSelection)
+        console.log("Computer:" +computerSelection)
+
+        playRound(humanSelection, computerSelection)  
+    }
+    
+    return{
+        humanScore: humanScore,
+        computerScore: computerScore
+    }
+}
+
+console.log(playGame())

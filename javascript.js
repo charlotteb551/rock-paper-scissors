9dc901c379f66b3046f9637a3c762f16ebf41122
charlotteb551 <295230572+charlotteb551@users.noreply.github.com>
2026-08-19 17:31:12 +0100

    Add button controls for RPS game

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//Gets Computers Selection
 function getComputerChoice(){
     const a = Math.random();
     if (a<1/3){
@@ -9,71 +10,62 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let b = prompt("Pick Rock, Paper or Scissors:");
-    return b.toLowerCase();
-}
+//Scores
+let humanScore = 0;
+let computerScore = 0;
 
-
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice){
-        if(humanChoice === "rock"){
-            if(computerChoice === "rock"){
-                console.log("It's a draw!");
-            }else if(computerChoice === "paper"){
-                console.log("You lose!");
-                computerScore += 1;
-            }else{
-                console.log("You win!");
-                humanScore += 1;
-            }
-        }else if(humanChoice === "paper"){
-            if(computerChoice === "paper"){
-                console.log("It's a draw!");
-            }else if(computerChoice === "scissors"){
-                console.log("You lose!");
-                computerScore += 1;
-            }else{
-                console.log("You win!");
-                humanScore += 1;
-            }
+//Plays one round
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === "rock"){
+        if(computerChoice === "rock"){
+            console.log("It's a draw!");
+        }else if(computerChoice === "paper"){
+            console.log("You lose!");
+            computerScore += 1;
         }else{
-            if(computerChoice === "scissors"){
-                console.log("It's a draw!");
-            }else if(computerChoice === "rock"){
-                console.log("You lose!");
-                computerScore += 1;
-            }else{
-                console.log("You win!");
-                humanScore += 1;
-            }
+            console.log("You win!");
+            humanScore += 1;
+        }
+    }else if(humanChoice === "paper"){
+        if(computerChoice === "paper"){
+            console.log("It's a draw!");
+        }else if(computerChoice === "scissors"){
+            console.log("You lose!");
+            computerScore += 1;
+        }else{
+            console.log("You win!");
+            humanScore += 1;
+        }
+    }else{
+        if(computerChoice === "scissors"){
+            console.log("It's a draw!");
+        }else if(computerChoice === "rock"){
+            console.log("You lose!");
+            computerScore += 1;
+        }else{
+            console.log("You win!");
+            humanScore += 1;
         }
     }
 
-    for (let i=0; i<5;i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        console.log("You:"+humanSelection)
-        console.log("Computer:" +computerSelection)
-
-        playRound(humanSelection, computerSelection)  
-    }
-    
-    console.log("Your Score:" + humanScore)
-    console.log("Computer's Score:" + computerScore)
-
-    if (humanScore > computerScore) {
-        console.log("You won overall!");
-    } else if (computerScore > humanScore) {
-        console.log("You lost overall!");
-    } else {
-        console.log("It's an overall draw!");
-    }
+    console.log("Your Score:" + humanScore);
+    console.log("Computer's Score:" + computerScore);
 
 }
 
-playGame()
+//Getting buttons and adding event listeners
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+
+rockButton.addEventListener("click", () =>{
+    playRound("rock",getComputerChoice());
+});
+
+paperButton.addEventListener("click", () =>{
+    playRound("paper",getComputerChoice());
+});
+
+scissorsButton.addEventListener("click", () =>{
+    playRound("scissors",getComputerChoice());
+});
